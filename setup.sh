0ed1e2d7ff1c30361c928b8c9dcf5c9e46fe4c6c
spaske00 <spaskeasm@gmail.com>
2025-01-10 09:17:16 +0100

    Move libraries to engine/libs

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-sudo apt install clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen
+sudo add-apt-repository universe
+sudo apt update
+sudo apt install g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen
+
+glxinfo | grep OpenGL
