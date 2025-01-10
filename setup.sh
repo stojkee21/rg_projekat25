3452d6e7383c4ffce4776024bf4b5f92396d9113
spaske00 <spaskeasm@gmail.com>
2025-01-10 12:08:49 +0100

    Add graphviz to setup.sh for doxygen graph

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo add-apt-repository universe
 sudo apt update
-sudo apt install g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen
+sudo apt install g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz
 
 glxinfo | grep OpenGL
