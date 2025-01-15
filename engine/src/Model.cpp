95eb6ae186cd40ec01d17596ef19b35160486454
spaske00 <spaskeasm@gmail.com>
2025-01-15 16:15:25 +0100

    Activate shader before draw by default

--- engine/src/Model.cpp
+++ engine/src/Model.cpp
@@ -1,9 +1,11 @@
 
 #include <engine/resources/Model.hpp>
+#include <engine/resources/Shader.hpp>
 
 namespace engine::resources {
 
     void Model::draw(const Shader *shader) {
+        shader->use();
         for (auto &mesh: m_meshes) {
             mesh.draw(shader);
         }
