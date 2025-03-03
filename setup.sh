8224ca9a05221b8e3eb7fe49e08b9687ad1e7385
spaske00 <spaskeasm@gmail.com>
2025-03-03 21:30:52 +0100

    Update setup.sh script with missing packages for lubuntu and configure default cc/c++.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
-sudo add-apt-repository universe
+sudo add-apt-repository universe ppa:ubuntu-toolchain-r/test
 sudo apt update
-sudo apt install g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz
+sudo apt install pkg-config g++-13 clang-format clang-tidy cmake git build-essential libwayland-dev libxkbcommon-dev xorg-dev libgl1-mesa-dev mesa-common-dev mesa-utils doxygen graphviz
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 10
+sudo update-alternatives --set cc /usr/bin/gcc
+sudo update-alternatives --set c++ /usr/bin/g++
+
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
 
 glxinfo | grep OpenGL
