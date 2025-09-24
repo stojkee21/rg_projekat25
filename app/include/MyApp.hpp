5ef25aa2b09fc7133bdd0898c116af68406ad112
Nemanja Stojadinović <stojadinovicn8@gmail.com>
2025-09-24 10:37:22 +0200

    First commit: first configure

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by matfrg on 9/21/25.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+
+}
+
+#endif //MYAPP_HPP
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by matfrg on 9/21/25.
+//
+#include  <MyApp.hpp>
+#include <spdlog/spdlog.h>
+
+namespace app {
+
+void MyApp::app_setup() { spdlog::info("App setup complete"); }
+
+}
