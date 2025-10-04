7ae705cdcb62cae21a68d6a97cf6405105f5dd4f
Nemanja <stojkee21>
2025-10-04 15:52:48 +0200

    Dodat apartment model.

--- app/include/MainController.h
+++ app/include/MainController.h
@@ -15,7 +15,11 @@ class MainController : public engine::core::Controller {
 
     void draw_police_station();
 
+    void begin_draw() override;
+
     void draw() override;
+
+    void end_draw() override;
 };
 
 }// namespace app
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -6,8 +6,14 @@
 
 #include "spdlog/spdlog.h"
 
+#include <engine/graphics/GraphicsController.hpp>
+
 namespace app {
-void MainController::initialize() { spdlog::info("MainController intialized."); }
+void MainController::initialize() {
+    spdlog::info("MainController intialized.");
+    engine::graphics::OpenGL::enable_depth_testing();
+
+}
 
 bool MainController::loop() {
     auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
@@ -21,13 +27,35 @@ bool MainController::loop() {
 }
 
 void MainController::draw_police_station() {
-    // Model
+
     auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
-    engine::resources::Model *model = resources->model("police_station");
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+
+    // Model
+    engine::resources::Model *apartment = resources->model("apartment");
     // Shader
     engine::resources::Shader *shader = resources->shader("basic");
-    model->draw(shader);
+
+    shader->use();
+
+    shader->set_mat4("projection", graphics->projection_matrix());
+    shader->set_mat4("view", graphics->camera()->view_matrix());
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -13.0f));
+    model = glm::scale(model, glm::vec3(0.3f));
+
+    shader->set_mat4("model", model);
+
+    apartment->draw(shader);
 }
 
+void MainController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
+
 void MainController::draw() { draw_police_station(); }
+
+void MainController::end_draw() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+}
 }// namespace app
